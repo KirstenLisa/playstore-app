ca7580b316ea1a3f4d379ceefab319bd47259e51
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-11-19 19:34:43 +0200

    genres filter not working

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const apps = require('./playstore-data.js');
 
 app.get('/apps', (req, res) => {
     const { genre, sort } = req.query;
-  
+    let results = apps
+
 
     if (genre) {
       if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
@@ -18,29 +19,23 @@ app.get('/apps', (req, res) => {
         .status(400)
         .send('Genre must be Action, Puzzle, Strategy, Casual, Arcade, Card')
       }
+      results = apps.filter(app =>
+      app["Genres"].toLowerCase().indexOf(genre.toLowerCase()) !== -1 
+      );
     }
-     let results = []
-    if (genre) {
-       results = apps.filter(app =>
-        app["Genres"].toLowerCase().indexOf(genre.toLowerCase()) ! == -1 
-       );
-      } 
-    //let results = apps;
-
+    
+    
     if (sort) {
       if (!['rating', 'app'].includes(sort)) {
         return res
         .status(400)
         .send('Sort must be one of rating or app');
             }
-          }
-    //sort by rating
-    if (sort) {
       results
-        .sort((a, b) => {
-          return a["Rating"] > b["Rating"] ? 1: a["Rating"] < b["Rating"] ? -1 : 0;
-            });
-          }
+      .sort((a, b) => {
+      return a["Rating"] > b["Rating"] ? 1: a["Rating"] < b["Rating"] ? -1 : 0;
+        });
+        }      
   
     res
       .json(results);
