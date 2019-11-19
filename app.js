c85deb83728bf291af45b76b8e3cf9db76cbbae7
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-11-19 09:39:38 +0200

    finished with issues

--- app.js
+++ app.js
@@ -9,29 +9,8 @@ app.use(cors());
 const apps = require('./playstore-data.js');
 
 app.get('/apps', (req, res) => {
-    const { genre="", sort } = req.query;
+    const { genre, sort } = req.query;
   
-    if (sort) {
-      if (!['rating', 'app'].includes(sort)) {
-        return res
-          .status(400)
-          .send('Sort must be one of rating or app');
-      }
-    }
-  
-    let results = apps
-          .filter(app =>
-              app
-                .genres
-                .toLowerCase()
-                .includes(genre.toLowerCase()));
-  
-    if (sort) {
-      results
-        .sort((a, b) => {
-          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-      });
-    }
 
     if (genre) {
       if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
@@ -40,10 +19,36 @@ app.get('/apps', (req, res) => {
         .send('Genre must be Action, Puzzle, Strategy, Casual, Arcade, Card')
       }
     }
+
+    if (genre) {
+      let results = apps
+      .filter(app =>
+        app
+          .Genres
+          .toLowerCase()
+          .includes(genre.toLowerCase()));
+      } let results = apps;
+
+    if (sort) {
+      if (!['rating', 'app'].includes(sort)) {
+        return res
+        .status(400)
+        .send('Sort must be one of rating or app');
+            }
+          }
+    
+    
+    if (sort) {
+      results
+        .sort((a, b) => {
+          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+            });
+          }
   
     res
       .json(results);
   });
+
   
   app.listen(8000, () => {
     console.log('Server started on PORT 8000');
--- google-play-client/src/playstore/playstore.js
+++ google-play-client/src/playstore/playstore.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Playstore = (props) => {
   return (
     <div className="app">
-      <h2>{ props.app }</h2>
-      <div className="app-rating"> { props.rating }</div>
-      <div className="app-genres"> { props.genres }
+      <h2>{ props.App }</h2>
+      <div className="app-rating"> { props.Rating }</div>
+      <div className="app-genres"> { props.Genres }
       </div>
       <div className="app-type">{ props.type }</div>
     </div>
